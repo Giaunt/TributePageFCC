659976123bcdc3d849e0fc115ea9073ff53ab7cc
Erica <ericakarrfalt@hotmail.com>
2022-02-15 20:07:50 -0500

    added updated files 2/15/22

---
+++ TributePageRobertKarrfalt.js
@@ -0,0 +1,5 @@
+// coded by @ChaituVR
+//edit and adapted by Erica Karrfalt
+const projectName = 'tribute-page';
+// added the following code from the FCC project instructions
+localStorage.setItem('example_project', 'Tribute Page');
